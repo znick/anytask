440b5fe725fb0ea274b0cbbf1738ea2e94f60e34
Aleksandr Bukin <gebetix@gmail.com>
2015-06-25 01:42:39 +0500

    sync author of review/replay and author of comment in issue

--- anytask/issues/views.py
+++ anytask/issues/views.py
@@ -59,13 +59,15 @@ def message_from_rb(request, review_id):
             break
 
     if request.method == 'POST':
-        value = u'Добавлен новый комментарий в <a href="{1}/r/{0}">Review \
+        value = u'<strong>Добавлен новый комментарий в <a href="{1}/r/{0}">Review \
                   request {0}</a>'.format(review_id,settings.RB_API_URL)+': \n'
-        value += u'<a href="{0}">Комментарий к коду</a>'.format(settings.RB_API_URL+request.POST.get('diff',''))
+        if request.POST.get('diff-url',0):
+            value += u'<a href="{0}">Комментарий к коду</a> '.format(settings.RB_API_URL+request.POST.get('diff-url',''))
+        value += request.POST.get('diff','')
         value += request.POST.get('body_top','') 
         value += request.POST.get('body_bottom','')
         field = get_object_or_404(IssueField, name='comment')
-        author = get_object_or_404(User, username='anytask')
+        author = get_object_or_404(User, username=request.POST.get('author',''))
         event = issue.create_event(field, author=author)
         event.value = value
         event.save()
