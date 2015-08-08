4e5d9cb89256ceb2aeb9c81efc61846d33b5e8e9
Aleksandr Bukin <gebetix@gmail.com>
2015-08-09 00:32:41 +0500

    change cron.d - add new paths remove useless .dirs

--- debian/anytask.cron.d
+++ debian/anytask.cron.d
@@ -7,8 +7,8 @@
 MAILTO=gebetix@yandex-team.ru
 HOME=/opt/anytask
 
-0   *   *   *   *  anytask     source /opt/anytask-env/bin/activate && cd /opt/anytask/anytask && python manage.py check_task_taken_expires --settings=settings_production
-0   2   *   *   *  anytask     source /opt/anytask-env/bin/activate && cd /opt/anytask/anytask && python manage.py cleanup --settings=settings_production
-0   3   *   *   *  anytask     source /opt/anytask-env/bin/activate && cd /opt/anytask/anytask && python manage.py check_invite_expires --settings=settings_production
-0   4   *   *   *  anytask     source /opt/anytask-env/bin/activate && cd /opt/anytask/anytask && python manage.py cleanupregistration --settings=settings_production
-*/5 *   *   *   *  anytask     source /opt/anytask-env/bin/activate && cd /opt/anytask/anytask && python manage.py check_contest
+0   *   *   *   *  anytask     source /usr/share/python/anytask/bin/activate && cd /usr/share/python/anytask/lib/python2.7/site-packages/Anytask-0.0.0-py2.7.egg/anytask && python manage.py check_task_taken_expires --settings=settings_production
+0   2   *   *   *  anytask     source /usr/share/python/anytask/bin/activate && cd /usr/share/python/anytask/lib/python2.7/site-packages/Anytask-0.0.0-py2.7.egg/anytask && python manage.py cleanup --settings=settings_production
+0   3   *   *   *  anytask     source /usr/share/python/anytask/bin/activate && cd /usr/share/python/anytask/lib/python2.7/site-packages/Anytask-0.0.0-py2.7.egg/anytask && python manage.py check_invite_expires --settings=settings_production
+0   4   *   *   *  anytask     source /usr/share/python/anytask/bin/activate && cd /usr/share/python/anytask/lib/python2.7/site-packages/Anytask-0.0.0-py2.7.egg/anytask && python manage.py cleanupregistration --settings=settings_production
+*/5 *   *   *   *  anytask     source /usr/share/python/anytask/bin/activate && cd /usr/share/python/anytask/lib/python2.7/site-packages/Anytask-0.0.0-py2.7.egg/anytask && python manage.py check_contest
