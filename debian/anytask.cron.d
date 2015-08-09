22b4d3ca17b1dc1e0affadd848714d90d65dd473
Aleksandr Bukin <gebetix@gmail.com>
2015-08-09 13:47:53 +0500

    fix cron.d

--- debian/anytask.cron.d
+++ debian/anytask.cron.d
@@ -8,7 +8,6 @@ MAILTO=gebetix@yandex-team.ru
 HOME=/home/anytask/
 SHELL=/bin/bash
 
-0   *   *   *   *  anytask  source /usr/share/python/anytask/bin/activate && cd /usr/share/python/anytask/lib/python2.7/site-packages/Anytask-0.0.0-py2.7.egg/anytask && python manage.py check_task_taken_expires --settings=settings_production
 0   2   *   *   *  anytask  source /usr/share/python/anytask/bin/activate && cd /usr/share/python/anytask/lib/python2.7/site-packages/Anytask-0.0.0-py2.7.egg/anytask && python manage.py cleanup --settings=settings_production
 0   3   *   *   *  anytask  source /usr/share/python/anytask/bin/activate && cd /usr/share/python/anytask/lib/python2.7/site-packages/Anytask-0.0.0-py2.7.egg/anytask && python manage.py check_invite_expires --settings=settings_production
 0   4   *   *   *  anytask  source /usr/share/python/anytask/bin/activate && cd /usr/share/python/anytask/lib/python2.7/site-packages/Anytask-0.0.0-py2.7.egg/anytask && python manage.py cleanupregistration --settings=settings_production
