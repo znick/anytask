83447cf83677617b48d20d7da0841620327dd431
Aleksandr Bukin <gebetix@gmail.com>
2015-08-09 06:23:32 +0500

    fixes

--- debian/anytask.cron.d
+++ debian/anytask.cron.d
@@ -1,14 +1,15 @@
 #
-# Regular cron jobs for the yandex-anytask package
+# Regular cron jobs for the yanytask package
 #
 # ТЫ БУДЕШЬ ОСТАВЛЯТЬ ПУСТУЮ СТРОКУ В КОНЦЕ ЭТОГО ФАЙЛА
 #
 
 MAILTO=gebetix@yandex-team.ru
-HOME=/opt/anytask
+HOME=/home/anytask/
 
-0   *   *   *   *  anytask     source /usr/share/python/anytask/bin/activate && cd /usr/share/python/anytask/lib/python2.7/site-packages/Anytask-0.0.0-py2.7.egg/anytask && python manage.py check_task_taken_expires --settings=settings_production
-0   2   *   *   *  anytask     source /usr/share/python/anytask/bin/activate && cd /usr/share/python/anytask/lib/python2.7/site-packages/Anytask-0.0.0-py2.7.egg/anytask && python manage.py cleanup --settings=settings_production
-0   3   *   *   *  anytask     source /usr/share/python/anytask/bin/activate && cd /usr/share/python/anytask/lib/python2.7/site-packages/Anytask-0.0.0-py2.7.egg/anytask && python manage.py check_invite_expires --settings=settings_production
-0   4   *   *   *  anytask     source /usr/share/python/anytask/bin/activate && cd /usr/share/python/anytask/lib/python2.7/site-packages/Anytask-0.0.0-py2.7.egg/anytask && python manage.py cleanupregistration --settings=settings_production
-*/5 *   *   *   *  anytask     source /usr/share/python/anytask/bin/activate && cd /usr/share/python/anytask/lib/python2.7/site-packages/Anytask-0.0.0-py2.7.egg/anytask && python manage.py check_contest
+0   *   *   *   *  anytask  source /usr/share/python/anytask/bin/activate && cd /usr/share/python/anytask/lib/python2.7/site-packages/Anytask-0.0.0-py2.7.egg/anytask && python manage.py check_task_taken_expires --settings=settings_production
+0   2   *   *   *  anytask  source /usr/share/python/anytask/bin/activate && cd /usr/share/python/anytask/lib/python2.7/site-packages/Anytask-0.0.0-py2.7.egg/anytask && python manage.py cleanup --settings=settings_production
+0   3   *   *   *  anytask  source /usr/share/python/anytask/bin/activate && cd /usr/share/python/anytask/lib/python2.7/site-packages/Anytask-0.0.0-py2.7.egg/anytask && python manage.py check_invite_expires --settings=settings_production
+0   4   *   *   *  anytask  source /usr/share/python/anytask/bin/activate && cd /usr/share/python/anytask/lib/python2.7/site-packages/Anytask-0.0.0-py2.7.egg/anytask && python manage.py cleanupregistration --settings=settings_production
+*/5 *   *   *   *  anytask  source /usr/share/python/anytask/bin/activate && cd /usr/share/python/anytask/lib/python2.7/site-packages/Anytask-0.0.0-py2.7.egg/anytask && python manage.py check_contest
+
