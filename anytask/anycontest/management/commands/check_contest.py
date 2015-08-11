a8fe2cf81fe56fe7b184cfd272942fb8b6885823
Aleksandr Bukin <gebetix@gmail.com>
2015-08-11 16:43:40 +0500

    add possibility to change order rb and contest sending - together or contest first

--- anytask/anycontest/management/commands/check_contest.py
+++ anytask/anycontest/management/commands/check_contest.py
@@ -3,7 +3,7 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings
 from anycontest.common import check_submission, comment_verdict
-
+from anyrb.common import AnyRB
 from issues.models import Event, Issue
 
 class Command(BaseCommand):
@@ -11,8 +11,16 @@ class Command(BaseCommand):
 
     def handle(self, **options):
         for issue in Issue.objects.filter(status=Issue.STATUS_AUTO_VERIFICATION):
-            for event in issue.event_set.all():
-                if event.issue.get_byname('run_id') != '':
-                    got_verdict, verdict, comment = check_submission(issue)
-                    if got_verdict:
-                        comment_verdict(issue, verdict, comment)
+            run_id = issue.get_byname('run_id')
+            if run_id != '':
+                for event in issue.event_set.all().reverse():
+                    if event.value == run_id:
+                        got_verdict, verdict, comment = check_submission(issue)
+                        if got_verdict:
+                            if verdict and not issue.task.course.send_rb_and_contest_together and issue.task.course.rb_integrated:
+                                anyrb = AnyRB(event.get_previous_by_timestamp())
+                                anyrb.upload_review()
+                                comment += '\n' + \
+                                          u'<a href="{1}/r/{0}">Review request {0}</a>'. \
+                                          format(issue.get_byname('review_id'),settings.RB_API_URL)
+                            comment_verdict(issue, verdict, comment)
