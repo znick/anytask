8b96c414baa48595e675dd0107a4b5c6d6249646
Aleksandr Bukin <gebetix@gmail.com>
2015-08-20 12:53:19 +0500

    add link for run_id routing to report about submission

--- anytask/anycontest/management/commands/check_contest.py
+++ anytask/anycontest/management/commands/check_contest.py
@@ -12,15 +12,14 @@ class Command(BaseCommand):
     def handle(self, **options):
         for issue in Issue.objects.filter(status=Issue.STATUS_AUTO_VERIFICATION):
             run_id = issue.get_byname('run_id')
-            if run_id != '':
-                for event in issue.event_set.all().reverse():
-                    if event.value == run_id:
-                        got_verdict, verdict, comment = check_submission(issue)
-                        if got_verdict:
-                            if verdict and not issue.task.course.send_rb_and_contest_together and issue.task.course.rb_integrated:
-                                anyrb = AnyRB(event.get_previous_by_timestamp())
-                                anyrb.upload_review()
-                                comment += '\n' + \
-                                          u'<a href="{1}/r/{0}">Review request {0}</a>'. \
-                                          format(issue.get_byname('review_id'),settings.RB_API_URL)
-                            comment_verdict(issue, verdict, comment)
+            for event in issue.event_set.all().reverse():
+                if event.value == run_id:
+                    got_verdict, verdict, comment = check_submission(issue)
+                    if got_verdict:
+                        if verdict and not issue.task.course.send_rb_and_contest_together and issue.task.course.rb_integrated:
+                            anyrb = AnyRB(event.get_previous_by_timestamp())
+                            anyrb.upload_review()
+                            comment += '\n' + \
+                                      u'<a href="{1}/r/{0}">Review request {0}</a>'. \
+                                      format(issue.get_byname('review_id'),settings.RB_API_URL)
+                        comment_verdict(issue, verdict, comment)
