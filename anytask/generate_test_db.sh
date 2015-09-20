3c6982091d52b6593787ff68d588b9420a8c9a8b
Nikolay Zhuravlev <znick@znick.ru>
2015-09-20 15:57:21 +0500

    Fix paths

--- anytask/generate_test_db.sh
+++ anytask/generate_test_db.sh
@@ -2,7 +2,7 @@
 
 [[ -f sqlite3.db ]] && echo "sqlite3.db already exists!" && exit 1
 
-/usr/share/python/anytask/bin/python manage.py syncdb --noinput
-/usr/share/python/anytask/bin/python manage.py migrate
-/usr/share/python/anytask/bin/python manage.py createsuperuser --username=anytask --email=anytask@yandex.ru --noinput
-echo 'from django.contrib.auth.models import User ; user=User.objects.get(username="anytask") ; user.set_password("P@ssw0rd") ; user.save() ; print "Password changed"' | /usr/share/python/anytask/bin/python manage.py shell --plain
+./manage.py syncdb --noinput
+./manage.py migrate
+./manage.py createsuperuser --username=anytask --email=anytask@yandex.ru --noinput
+echo 'from django.contrib.auth.models import User ; user=User.objects.get(username="anytask") ; user.set_password("P@ssw0rd") ; user.save() ; print "Password changed"' | ./manage.py shell --plain
