63d7a9223fad2eeec19568b228adb315010f6394
Aleksandr Bukin <gebetix@gmail.com>
2016-01-25 14:55:21 +0500

    big refactoring part 2

--- anytask/courses/urls.py
+++ anytask/courses/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import patterns, url
 
-from courses.views import SubmitReview, SubmitReviewForm
 from filemanager import path_end
 
 urlpatterns = patterns('courses.views',
@@ -16,9 +15,6 @@ urlpatterns = patterns('courses.views',
     url(r'^statistics/(?P<course_id>\d+)', 'course_statistics'),
     url(r'^tasks_description/(?P<course_id>\d+)', 'tasks_description'),
     url(r'^set_spectial_course_attend', 'set_spectial_course_attend'),
-    url(r'^submit_review_form/(?P<task_id>\d+)/(?P<svn_path>.*)', SubmitReviewForm.as_view()),
-    url(r'^submit_pdf_gr_form/(?P<task_id>\d+)', 'submit_pdf_gr_form', name='submit_pdf_gr_form'),
-    url(r'^submit_review/(?P<task_id>\d+)$', SubmitReview.as_view(), name='submit_review'),
     url(r'^queue_tasks_to_check/(?P<course_id>\d+)', 'queue_tasks_to_check'),
     url(r'^ajax_get_transcript/(?P<course_id>\d+)', 'ajax_get_transcript'),
     url(r'^tasks_list/(?P<course_id>\d+)', 'tasks_list'),
