8d684921f3471bec93c5b3d12017216e03d2844a
Aleksandr Bukin <gebetix@gmail.com>
2016-01-25 21:02:10 +0500

    remove useless spaces

--- anytask/tasks/models.py
+++ anytask/tasks/models.py
@@ -83,11 +83,11 @@ class Task(models.Model):
 
         return (True, u'')
 
-    def user_can_cancel_task(self, user):      
-        if user.is_anonymous() or self.is_hidden:      
-            return False       
+    def user_can_cancel_task(self, user):
+        if user.is_anonymous() or self.is_hidden:
+            return False
         if TaskTaken.objects.filter(task=self).filter(user=user).filter(status=TaskTaken.STATUS_TAKEN).count() != 0:       
-            return True        
+            return True
         return False
 
     def user_can_score_task(self, user):
