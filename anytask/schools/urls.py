f5bcd6b5da47477e8d7078af3662e3eaa543de0a
Aleksandr Bukin <gebetix@gmail.com>
2016-02-01 20:34:14 +0500

    change regexp in urls.py for schools app

--- anytask/schools/urls.py
+++ anytask/schools/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('schools.views',
-    url(r'^(?P<school_link>\d+)$', 'school_page'),
+    url(r'^(?P<school_link>\w+)$', 'school_page'),
 
 )
