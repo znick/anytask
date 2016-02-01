91869c0eb660f2a975d6dc0bf5a0a185d4a45ff3
Aleksandr Bukin <gebetix@gmail.com>
2016-02-01 14:41:38 +0500

    some fixes for school app

--- anytask/schools/models.py
+++ anytask/schools/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from courses.models import Course
+from courses.models import School
 
 # Create your models here.
 
-class Course(models.Model):
+class School(models.Model):
 
     name = models.CharField(max_length=254, db_index=True, null=False, blank=False)
     courses = models.ManyToManyField(Course, null=True, blank=True)
