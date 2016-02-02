d37b0102d3b06e6fd9873ab2fd7a4418eea2acb1
Aleksandr Bukin <gebetix@gmail.com>
2016-02-02 11:54:40 +0500

    some visual fixes for views and templates of schools

--- anytask/index/views.py
+++ anytask/index/views.py
@@ -6,14 +6,14 @@ from schools.models import School
 
 def index(request):
 
-    schools = School.objects.all()
+    schools = School.objects.all().order_by('name')
 
     #current_year = get_current_year()
     #courses = Course.objects.filter(is_active=True).order_by('name')
     #archive_courses = Course.objects.exclude(is_active=True).order_by('year', 'name')
 
     context = {
-        'schools'       : schools,
+        'schools' : schools,
     }
    
     return render_to_response('index.html', context, context_instance=RequestContext(request))
