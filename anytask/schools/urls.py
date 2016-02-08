1e85d684a7eb9e980ea681cb21af71685a1ea63f
Aleksandr Bukin <gebetix@gmail.com>
2016-02-09 00:13:27 +0500

    add archive_page

--- anytask/schools/urls.py
+++ anytask/schools/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('schools.views',
     url(r'^(?P<school_link>\w+)$', 'school_page'),
+    url(r'^(?P<school_link>\w+)/archive/$', 'archive_page'),
 
 )
