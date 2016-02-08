c237f9246bcba779988c948079f0d19fe4288f85
Aleksandr Bukin <gebetix@gmail.com>
2016-02-09 00:33:44 +0500

    add archive view

--- anytask/schools/views.py
+++ anytask/schools/views.py
@@ -13,7 +13,7 @@ from schools.models import School
 @login_required
 def school_page(request, school_link):
     school = get_object_or_404(School, link=school_link)
-    courses = school.courses.all().order_by('name')
+    courses = school.courses.all().filter(is_active=True).order_by('-name')
 
     context = {
         'school'  : school,
@@ -21,3 +21,15 @@ def school_page(request, school_link):
     }
    
     return render_to_response('school_page.html', context, context_instance=RequestContext(request))
+
+@login_required
+def archive_page(request, school_link):
+    school = get_object_or_404(School, link=school_link)
+    courses = school.courses.all().filter(is_active=False).order_by('-name')
+
+    context = {
+        'school'  : school,
+        'courses' : courses,
+    }
+   
+    return render_to_response('archive_page.html', context, context_instance=RequestContext(request))
