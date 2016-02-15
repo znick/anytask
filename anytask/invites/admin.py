efc308d0a8048391ddccafc72fb7b33a4ea7a0f1
Aleksandr Bukin <gebetix@gmail.com>
2016-02-16 02:15:13 +0500

    tabs NO, spaces YES

--- anytask/invites/admin.py
+++ anytask/invites/admin.py
@@ -2,7 +2,7 @@ from invites.models import Invite
 from django.contrib import admin
 
 class InviteAdmin(admin.ModelAdmin):
-	filter_horizontal = ('invited_users',)
+    filter_horizontal = ('invited_users',)
     list_display = ('generated_by', 'group', 'key')
     list_filter = ('group',)
     search_fields = ('generated_by__username', 'group__name', 'key')
