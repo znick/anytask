9e13ef895088de3816aa62a6f55f2864fbdaf63f
Nikolay Zhuravlev <znick@znick.ru>
2016-03-04 02:08:23 +0500

    EasyCi. Add set_unchecked admin task.

--- anytask/easy_ci/admin.py
+++ anytask/easy_ci/admin.py
@@ -2,14 +2,22 @@ from django.contrib import admin
 
 from easy_ci.models import EasyCiTask, EasyCiCheck
 
+
+def set_unchecked(modeladmin, request, queryset):
+    queryset.update(checked=False)
+set_unchecked.short_description = "Mark tasks unchecked"
+
 class EasyCiTaskAdmin(admin.ModelAdmin):
     list_display = ('student', 'task', 'added_time', 'update_time')
     list_filter = ("task__cource", "task__cource__year", "student__group")
     search_fields = ('student', 'task', 'data')
+    actions = [set_unchecked]
+
 
 admin.site.register(EasyCiTask, EasyCiTaskAdmin)
 
 class EasyCiCheckAdmin(admin.ModelAdmin):
     list_display = ('easy_ci_task', 'exit_status')
+    list_filter = ("easy_ci_task__task", "easy_ci_task__task__cource__year", "easy_ci_task__student__group")
 
 admin.site.register(EasyCiCheck, EasyCiCheckAdmin)
