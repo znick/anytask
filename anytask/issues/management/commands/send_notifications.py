d4df9dc21b00aa3f9200b359216a0d5b1cdfd7f1
Nikolay Makhalin <makhalin@yandex-team.ru>
2016-04-14 14:31:27 +0500

    add course name to subject

--- anytask/issues/management/commands/send_notifications.py
+++ anytask/issues/management/commands/send_notifications.py
@@ -19,8 +19,10 @@ class Command(BaseCommand):
 
         for issue in issues:
             events = issue.get_history().exclude(sended_notify=True)
-            message_header = 'http://' + domain + \
-                              issue.get_absolute_url() + '\n\n'
+            message_header = u'Здравствуйте, {0}.\n\n' + \
+                             u'В задаче "{1}", в который вы являетесь {2}, появились новые комментарии: \n' + \
+                             'http://' + domain + \
+                             issue.get_absolute_url() + '\n\n'
 
             message_body = []
             empty_message = True
@@ -39,9 +41,10 @@ class Command(BaseCommand):
             message_body.append(u'С уважением,')
             message_body.append(u'команда Anytask')
             message_text = message_header + '\n'.join(message_body)
-
-            subject = u'{0} {1} {2} {3}'.\
-                format(issue.task.course, issue,  issue.student.last_name, issue.student.first_name)
+            print (message_text);
+            subject = u'Курс: {0} | Задача: {1} | Студент: {2} {3}'.\
+                format(issue.task.course, issue.task.title, issue.student.last_name, issue.student.first_name)
+            print (subject)
             from_email = settings.DEFAULT_FROM_EMAIL
 
             def get_message(email):
@@ -50,12 +53,18 @@ class Command(BaseCommand):
             notify_messages = []
             if not empty_message:
                 if issue.student.email:
+                    message_text = message_text.\
+                        format(issue.student.first_name, issue.task.title, u'студентом')
                     notify_messages.append(get_message(issue.student.email))
                 if issue.responsible:
                     if issue.responsible.email:
+                        message_text = message_text.\
+                            format(issue.responsible.first_name, issue.task.title, u'проверяющим')
                         notify_messages.append(get_message(issue.responsible.email))
                 for follower in issue.followers.all():
                     if follower.email:
+                        message_text = message_text.\
+                            format(follower.first_name, issue.task.title, u'наблюдателем')
                         notify_messages.append(get_message(follower.email))
                 send_mass_mail(notify_messages)
                 time.sleep(1)
