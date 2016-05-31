368dc1bcbdbda2ea14dd9cdaab52f7a4507ae0fc
Nikolay Makhalin <makhalin@yandex-team.ru>
2016-05-31 19:01:28 +0500

    add focusout method for mark form

--- anytask/tasks/models.py
+++ anytask/tasks/models.py
@@ -32,9 +32,9 @@ class Task(models.Model):
     TYPE_FULL = 'All'
     TYPE_SIMPLE = 'Only mark'
     TASK_TYPE_CHOICES = (
-        (TYPE_FULL, u'Все'),
-        (TYPE_SIMPLE, u'Только оценка'),
-    )
+        (TYPE_FULL, u'с обсуждением'),
+        (TYPE_SIMPLE, u'без обсуждения'),
+    )   
     type = models.CharField(db_index=False, max_length=128, choices=TASK_TYPE_CHOICES, default=TYPE_FULL)
 
     added_time = models.DateTimeField(auto_now_add=True, default=datetime.now)
