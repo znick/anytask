99b381654db8827ac9ce58e71554fddbb5ef465f
Nikolay Makhalin <makhalin@yandex-team.ru>
2016-09-07 15:23:17 +0500

    delete color for card header and active last tab if none

--- anytask/users/templatetags/table_func.py
+++ anytask/users/templatetags/table_func.py
@@ -7,3 +7,11 @@ register = template.Library()
 @register.filter(name='exist')
 def another_table_exist(d, index):
     return bool(d[int(not index)])
+
+
+@register.filter(name='has_item')
+def another_table_exist(d, item):
+    for x, y in d:
+        if x == item:
+            return True
+    return False
