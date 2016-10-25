e549f11802882ac18f54c496372b7de9b5af9a63
Nikolay Makhalin <makhalin@yandex-team.ru>
2016-10-25 18:50:55 +0500

    add better comment form on issue page and escape in contest response

--- anytask/tasks/templatetags/sanitize_html.py
+++ anytask/tasks/templatetags/sanitize_html.py
@@ -5,25 +5,25 @@ from django.utils.translation import ugettext as _
 
 register = template.Library()
 
-ALLOWED_TAGS = 'p i strong b u a h1 h2 h3 pre br img'
+ALLOWED_TAGS = 'p i strong b u a h1 h2 h3 pre br img blockquote'
 ALLOWED_TAGS_STRING = _("Allowed tags: ") + ALLOWED_TAGS
 
 def sanitize_html(value):
     valid_tags = ALLOWED_TAGS.split()
-    valid_attrs = 'href src'.split()
+    valid_attrs = 'href src target alt'.split()
 
     if not value:
         return ''
 
     soup = BeautifulSoup(value)
-    for comment in soup.findAll(
-        text=lambda text: isinstance(text, Comment)):
-        comment.extract()
-    for tag in soup.findAll(True):
-        if tag.name not in valid_tags:
-            tag.hidden = True
-        tag.attrs = [(attr, val) for attr, val in tag.attrs
-                     if attr in valid_attrs]
-    return soup.renderContents().decode('utf8').replace('javascript:', '').replace("\n",'<br/>')
+    # for comment in soup.findAll(
+    #     text=lambda text: isinstance(text, Comment)):
+    #     comment.extract()
+    # for tag in soup.findAll(True):
+    #     if tag.name not in valid_tags:
+    #         tag.hidden = True
+    #     tag.attrs = [(attr, val) for attr, val in tag.attrs
+    #                  if attr in valid_attrs]
+    return soup.renderContents().decode('utf8').replace('javascript:', '')
 
 register.filter('sanitize', sanitize_html)
