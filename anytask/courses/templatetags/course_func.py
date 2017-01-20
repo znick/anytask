3b65778bcff9e0838d6ffba4abc3950d16ae7691
Aleksandr Bukin <gebetix@gmail.com>
2017-01-20 15:02:07 +0500

    add status label + fixes

--- anytask/courses/templatetags/course_func.py
+++ anytask/courses/templatetags/course_func.py
@@ -1,5 +1,6 @@
 from django import template
 from issues.models import Issue
+from issues.model_issue_status import IssueStatus
 
 register = template.Library()
 
@@ -13,5 +14,8 @@ def get_score(task, user):
 
 
 @register.filter(name='get_status_label')
-def get_status_label(d, key):
-    return d.get(key).last_name + ' ' + d.get(key).first_name
+def get_status_label(task, user):
+    try:
+        return Issue.objects.get(task=task, student=user).status_field.color
+    except Exception as e:
+        return IssueStatus.COLOR_DEFAULT
