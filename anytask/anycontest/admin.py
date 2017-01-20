442f722a020d844ea01feee446158fd3924df758
kobolok <makhalinnick@gmail.com>
2017-01-20 19:53:39 +0500

    optimize admin view contest submission

--- anytask/anycontest/admin.py
+++ anytask/anycontest/admin.py
@@ -7,6 +7,6 @@ from anycontest.models import ContestSubmission
 
 class ContestSubmissionAdmin(admin.ModelAdmin):
     list_display = ('issue', 'run_id', 'author')
-    readonly_fields = ('create_time', 'update_time')
+    readonly_fields = ('issue', 'file', 'create_time', 'update_time')
 
 admin.site.register(ContestSubmission, ContestSubmissionAdmin)
