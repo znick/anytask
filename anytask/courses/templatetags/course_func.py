e475e298165f32618cc471bbeb09462474eba16c
Aleksandr Bukin <gebetix@gmail.com>
2017-01-23 18:28:11 +0500

    update tasks tab on course_page

--- anytask/courses/templatetags/course_func.py
+++ anytask/courses/templatetags/course_func.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django import template
 from issues.models import Issue
 from issues.model_issue_status import IssueStatus
@@ -13,9 +14,17 @@ def get_score(task, user):
         return 0
 
 
-@register.filter(name='get_status_label')
-def get_status_label(task, user):
+@register.filter(name='get_status_color')
+def get_status_color(task, user):
     try:
         return Issue.objects.get(task=task, student=user).status_field.color
     except Exception as e:
         return IssueStatus.COLOR_DEFAULT
+
+
+@register.filter(name='get_status_name')
+def get_status_name(task, user):
+    try:
+        return Issue.objects.get(task=task, student=user).status_field.name
+    except Exception as e:
+        return u"Новая"
