e5b3dd309996339f3df23ba82a5b25d2d9d57a7a
itjune <itjune@yandex-team.ru>
2017-02-07 13:21:27 +0500

    make sorting faster

--- anytask/invites/views.py
+++ anytask/invites/views.py
@@ -29,7 +29,7 @@ def generate_invites(request):
         if user in course.teachers.all():
             true_courses.append(course)
 
-    groups = sorted(set(groups.filter(course__in=true_courses)), key=lambda x: x.name)
+    groups = groups.filter(course__in=true_courses).distinct().order_by('name')
 
     context = {
         'groups'    : groups,
