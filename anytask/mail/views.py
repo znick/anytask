38efd2682df71b1acbc67580fd2aa82c0029100b
Nikolay Makhalin <makhalinnick@gmail.com>
2017-03-29 16:15:21 +0500

    ANYTASK-427: Добавить темплейты в систему рассылок (#132)
    * add email template
    
    * add new user status type
    
    * add new status type in filter
    
    * add mass mail options
    
    * add fulltext notification

--- anytask/mail/views.py
+++ anytask/mail/views.py
@@ -5,11 +5,17 @@ from django.http import HttpResponseRedirect, HttpResponseForbidden, HttpRespons
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template.context import RequestContext
 from django.utils.translation import ugettext as _
+from django.views.decorators.http import require_POST, require_GET
+from django.conf import settings
 
 from django.contrib.auth.models import User
 from mail.models import Message
 from courses.models import Course
 from groups.models import Group
+from users.models import UserProfile
+from users.model_user_status import get_statuses
+
+from mail.common import render_mail
 
 import json
 import datetime
@@ -30,10 +36,20 @@ MONTH = {
 }
 
 
+@require_GET
 @login_required
 def mail_page(request):
     user = request.user
     user_profile = user.get_profile()
+
+    users_from_staff_len = {}
+    if user.is_staff and 'from_staff' in request.GET and 'user_ids_send_mail_counter' in request.session:
+        key = 'user_ids_send_mail_' + request.GET['from_staff']
+        if key in request.session:
+            users_from_staff_len = {
+                'index': request.GET['from_staff'],
+                'length': len(request.session[key]) ,
+            }
 
     if user.is_staff:
         courses_teacher = Course.objects.filter(is_active=True)
@@ -41,21 +57,23 @@ def mail_page(request):
         courses_teacher = Course.objects.filter(teachers=user, is_active=True)
 
     context = {
+        "user": user,
         "user_profile": user_profile,
-        "courses_teacher": courses_teacher
+        "courses_teacher": courses_teacher,
+        'user_statuses': get_statuses(),
+        "users_from_staff_len": users_from_staff_len,
+        "snow_alert_message_fulltext": hasattr(settings, 'SEND_MESSAGE_FULLTEXT') and settings.SEND_MESSAGE_FULLTEXT,
     }
 
     return render_to_response('mail.html', context, context_instance=RequestContext(request))
 
 
+@require_GET
 @login_required
 def ajax_get_mailbox(request):
     response = dict()
     user = request.user
     user_profile = user.get_profile()
-
-    if request.method != "GET":
-        return HttpResponseForbidden()
 
     datatable_data = dict(request.GET)
 
@@ -67,7 +85,7 @@ def ajax_get_mailbox(request):
             user_profile.unread_messages.clear()
             user_profile.send_notify_messages.clear()
         else:
-            user_profile.unread_messages = user_profile.unread_messages\
+            user_profile.unread_messages = user_profile.unread_messages \
                 .exclude(id__in=datatable_data["make_read[]"])
             user_profile.send_notify_messages = user_profile.send_notify_messages \
                 .exclude(id__in=datatable_data["make_read[]"])
@@ -132,14 +150,12 @@ def format_date(date):
     return date_str
 
 
+@require_GET
 @login_required
 def ajax_get_message(request):
     response = dict()
     user = request.user
     user_profile = user.get_profile()
-
-    if request.method != "GET":
-        return HttpResponseForbidden()
 
     if "msg_id" not in request.GET:
         return HttpResponseForbidden()
@@ -156,27 +172,44 @@ def ajax_get_message(request):
         unread_count -= 1
 
     recipients_user = []
-    for recipient in message.recipients_user.all():
-        recipients_user.append({
-            "id": recipient.id,
-            "fullname": u'%s %s' % (recipient.last_name, recipient.first_name),
-            "url": recipient.get_absolute_url()
-        })
-
     recipients_group = []
-    for group in message.recipients_group.all():
-        recipients_group.append({
-            "id": group.id,
-            "name": group.name
-        })
-
     recipients_course = []
-    for course in message.recipients_course.all():
-        recipients_course.append({
-            "id": course.id,
-            "name": course.name,
-            "url": course.get_absolute_url(),
+    recipients_status = []
+
+    if message.hidden_copy and message.sender != user:
+        recipients_user.append({
+            "id": user.id,
+            "fullname": u'%s %s' % (user.last_name, user.first_name),
+            "url": user.get_absolute_url()
         })
+    else:
+        for recipient in message.recipients_user.all():
+            recipients_user.append({
+                "id": recipient.id,
+                "fullname": u'%s %s' % (recipient.last_name, recipient.first_name),
+                "url": recipient.get_absolute_url()
+            })
+        for group in message.recipients_group.all():
+            recipients_group.append({
+                "id": group.id,
+                "name": group.name
+            })
+        for course in message.recipients_course.all():
+            recipients_course.append({
+                "id": course.id,
+                "name": course.name,
+                "url": course.get_absolute_url(),
+            })
+        for status in message.recipients_status.all():
+            recipients_status.append({
+                "id": status.id,
+                "name": status.name
+            })
+
+    if message.sender != user or request.GET["mailbox"] == 'inbox':
+        text = render_mail(message, user)
+    else:
+        text = message.text
 
     response['sender'] = {
         "id": message.sender.id,
@@ -187,34 +220,47 @@ def ajax_get_message(request):
     response['recipients_user'] = recipients_user
     response['recipients_group'] = recipients_group
     response['recipients_course'] = recipients_course
+    response['recipients_status'] = recipients_status
     response['date'] = message.create_time.strftime("%d.%m.%y %H:%M:%S")
-    response['text'] = message.text
+    response['text'] = text
     response['unread_count'] = unread_count
 
     return HttpResponse(json.dumps(response),
                         content_type="application/json")
 
 
+@require_POST
 @login_required
 def ajax_send_message(request):
     response = dict()
     user = request.user
     user_profile = user.get_profile()
 
-    if request.method != "POST":
-        return HttpResponseForbidden()
-
     data = dict(request.POST)
+
+    hidden_copy = False
+    if 'hidden_copy' in data and data['hidden_copy'][0]:
+        hidden_copy = True
+
+    variable = False
+    if 'variable' in data and data['variable'][0]:
+        variable = True
 
     message = Message()
     message.sender = user
     message.title = data['new_title'][0]
     message.text = data['new_text'][0]
+    message.hidden_copy = hidden_copy
+    message.variable = variable
     message.save()
 
     recipients_ids = set()
-    if "new_recipients_user[]" in data:
-        message.recipients_user = data["new_recipients_user[]"]
+
+    if "new_recipients_user[]" in data or "new_recipients_preinit[]" in data:
+        users = data.get("new_recipients_user[]", [])
+        if "new_recipients_preinit[]" in data:
+            users += request.session.get('user_ids_send_mail_' + data["new_recipients_preinit[]"][0], [])
+        message.recipients_user = users
         recipients_ids.update(message.recipients_user.values_list('id', flat=True))
 
     group_ids = []
@@ -231,6 +277,12 @@ def ajax_send_message(request):
         for course in Course.objects.filter(id__in=data["new_recipients_course[]"]):
             for group in course.groups.exclude(id__in=group_ids).distinct():
                 recipients_ids.update(group.students.exclude(id=user.id).values_list('id', flat=True))
+
+    if "new_recipients_status[]" in data:
+        message.recipients_status = data["new_recipients_status[]"]
+
+        recipients_ids.update(UserProfile.objects.filter(user_status__in=data["new_recipients_status[]"])
+                              .values_list('user__id', flat=True))
 
     message.recipients = list(recipients_ids)
 
