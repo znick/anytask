ae61a879aa50e5a62bbcdd6c7bd549cd267fcb53
itjune <itjune@yandex-team.ru>
2017-04-27 18:29:59 +0500

    update po, refactor code

--- anytask/lessons/views.py
+++ anytask/lessons/views.py
@@ -44,12 +44,6 @@ def schedule_edit_page(request, lesson_id):
 
     if request.method == 'POST':
         return lesson_edit(request, lssn.course, lesson_id)
-    # students = set(lssn.visited_students.all())
-    # groups_required = []
-    # groups = lssn.groups.all()
-    # for group in groups:
-    #     if students.intersection(set(group.students.all())):
-    #         groups_required.append(group)
 
     schools = lssn.course.school_set.all()
 
@@ -58,9 +52,7 @@ def schedule_edit_page(request, lesson_id):
         'course': lssn.course,
         'lesson': lssn,
         'period_types': lssn.PERIOD_CHOICES,
-        # 'groups_required': groups_required,
         'school': schools[0] if schools else '',
-        # 'show_help_msg_lssn_group': True if groups_required else False
     }
 
     return render_to_response('lesson_edit.html', context, context_instance=RequestContext(request))
@@ -114,6 +106,7 @@ def set_params(params, course, group, user, schedule_id, lssn_date, lesson=None)
     lssn.group = group
     lssn.save()
     lssn.set_position()
+    return lssn.id
 
 
 def lesson_create(request, course):
@@ -122,27 +115,13 @@ def lesson_create(request, course):
     for group in params['lesson_groups']:
         schedule_id = int(datetime.datetime.now().strftime('%d%m%y%H%M%S') + str(group.id))
         for lssn_date in params['lesson_dates']:
-            set_params(params, course, group, user, schedule_id, lssn_date)
-            # lssn = Lesson()
-            # lssn.title = params['lesson_title']
-            # lssn.course = course
-            # lssn.schedule_id = schedule_id
-            # lssn.date_starttime = lssn_date[0]
-            # lssn.date_endtime = lssn_date[1]
-            # lssn.period = params['period']
-            # lssn.date_end = params['date_end']
-            # lssn.days = params['lesson_days']
-            # lssn.description = params['lesson_description']
-            # lssn.updated_by = user
-            # lssn.group = group
-            # lssn.save()
-            # lssn.set_position()
+            lssn_id = set_params(params, course, group, user, schedule_id, lssn_date)
 
     reversion.set_user(user)
     reversion.set_comment("Create lesson")
 
     return HttpResponse(json.dumps({'page_title': str(schedule_id) + ' | ' + course.name + ' | ' + str(course.year),
-                                    'redirect_page': '/lesson/edit/' + str(lssn.id)}),
+                                    'redirect_page': '/lesson/edit/' + str(lssn_id)}),
                         content_type="application/json")
 
 
@@ -179,82 +158,10 @@ def lesson_edit(request, course, lesson_id):
 
     for i, lssn_date in enumerate(params['lesson_dates']):
         set_params(params, course, group, user, schedule_id, lssn_date, None if not lesson_changed else lesson_changed[i])
-        # lssn = Lesson() if not lesson_changed else lesson_changed[i]
-        # lssn.title = params['lesson_title']
-        # lssn.course = course
-        # lssn.schedule_id = schedule_id
-        # lssn.date_starttime = lssn_date[0]
-        # lssn.date_endtime = lssn_date[1]
-        # lssn.period = params['period']
-        # lssn.date_end = params['date_end']
-        # lssn.days = params['lesson_days']
-        # lssn.description = params['lesson_description']
-        # lssn.updated_by = user
-        # lssn.group = group
-        # lssn.save()
-        # lssn.set_position()
 
     reversion.set_user(user)
     reversion.set_comment("Edit lesson")
 
     return HttpResponse(json.dumps({'page_title': str(schedule_id) + ' | ' + course.name + ' | ' + str(course.year),
                                     'redirect_page': None}),
-                        content_type="application/json")
-
-
-def lesson_create_or_edit(request, course, lesson_id=None):
-
-    # user = request.user
-    # lesson_title = request.POST['lesson_title'].strip()
-    # lesson_groups = Group.objects.filter(id__in=dict(request.POST)['lesson_group_id[]'])
-    # period = request.POST['period_type'].strip()
-    # date_starttime = datetime.datetime.strptime(request.POST['lesson_date_start'], '%d-%m-%Y %H:%M')
-    # date_endtime = datetime.datetime.strptime(request.POST['lesson_date_finish'], '%d-%m-%Y %H:%M')
-    # if period != 'Once':
-    #     date_end = datetime.datetime.strptime(request.POST['date_end'], '%d-%m-%Y %H:%M')
-    #     week_days = list(map(int, (dict(request.POST)['days[]'])))
-    #     lesson_dates = get_lesson_dates(date_starttime, date_endtime, date_end, week_days)
-    #     lesson_days = ','.join(dict(request.POST)['days[]'])
-    # else:
-    #     date_end = date_endtime
-    #     lesson_dates = [(date_starttime, date_endtime)]
-    #     lesson_days = ''
-    #
-    # lesson_description = request.POST['lesson_text'].strip()
-
-    create_new = True
-    schedule_id = False
-    if lesson_id:
-        lssn = get_object_or_404(Lesson, id=lesson_id)
-        schedule_id = lssn.schedule_id
-        if 'change_all' not in request.POST:
-            lesson_dates = [(date_starttime, date_endtime)]
-            create_new = False
-        else:
-            Lesson.objects.filter(schedule_id=schedule_id, date_starttime__gte=date_starttime.date()).delete()
-
-    for group in lesson_groups:
-        if not schedule_id:
-            schedule_id = int(datetime.datetime.now().strftime('%d%m%y%H%M%S') + str(group.id))
-        for lssn_date in lesson_dates:
-            lssn = Lesson() if create_new else lssn
-            lssn.title = lesson_title
-            lssn.course = course
-            lssn.schedule_id = schedule_id
-            lssn.date_starttime = lssn_date[0]
-            lssn.date_endtime = lssn_date[1]
-            lssn.period = period
-            lssn.date_end = date_end
-            lssn.days = lesson_days
-            lssn.description = lesson_description
-            lssn.updated_by = user
-            lssn.group = group
-            lssn.save()
-            lssn.set_position_in_new_group(group)
-
-    reversion.set_user(user)
-    reversion.set_comment("Create lesson")
-
-    return HttpResponse(json.dumps({'page_title': str(schedule_id) + ' | ' + course.name + ' | ' + str(course.year),
-                                    'redirect_page': '/lesson/edit/' + str(lesson_id) if not lesson_id else None}),
                         content_type="application/json")
