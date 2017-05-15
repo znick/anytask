e0119ca3c82e98ae8a8314f889b2c744d6a18830
Nikolay Makhalin <makhalinnick@gmail.com>
2017-05-15 11:20:20 +0500

    ANYTASK-513: Добавить ручку для пересылки пользователя в контест (#171)
    * add view contest result url
    
    * fix url cutting

--- anytask/admission/urls.py
+++ anytask/admission/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('admission.views',
                        url(r'^register$', 'register'),
                        url(r'^activate/(?P<activation_key>\w+)/', 'activate'),
+                       url(r'^results/(?P<username>.*)/', 'contest_results_redirect'),
                        # url(r'^decline/(?P<activation_key>\w+)/', 'decline'),
                        )
