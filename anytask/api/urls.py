92d390165be298c31d7bfa2028af5cafc73f342f
Nikolay Zhuravlev <znick@znick.ru>
2017-10-25 18:42:23 +0300

    Change API urls

--- anytask/api/urls.py
+++ anytask/api/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns('api.views',
-                       url(r'^v1/api/course/(?P<course_id>\d+)/issues$', 'get_issues'),
-                       url(r'^v1/api/issue/(?P<issue_id>\d+)$', 'get_issue'),
-                       url(r'^v1/api/add_comment/(?P<issue_id>\d+)$', 'add_comment'),
+                       url(r'^v1/course/(?P<course_id>\d+)/issues$', 'get_issues'),
+                       url(r'^v1/issue/(?P<issue_id>\d+)/add_comment$', 'add_comment'),
+                       url(r'^v1/issue/(?P<issue_id>\d+)$', 'get_issue'),
                        )
