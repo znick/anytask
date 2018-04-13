bf260a1cb93194c705a90f6d06a368941575a543
Volkov Denis <denchick1997@mail.com>
2018-04-13 23:33:44 +0500

    fix manage.py

--- anytask/manage.py
+++ anytask/manage.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 
 import locale
+import settings
 import sys
-import os
-import sys
+import imp
 
+from django.core.management import execute_manager
 
 locale.setlocale(locale.LC_ALL, '')
+
+try:
+    imp.find_module('settings')  # Assumed to be in the same directory.
+except ImportError:
+    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. "
+                     "It appears you've customized things.\n"
+                     "You'll have to run django-admin.py, passing it your settings module.\n" % __file__)
+    sys.exit(1)
 
 
 # To skip test_site_profile_not_available from django.contrib.auth.tests.models.ProfileTestCase
@@ -14,6 +23,4 @@ locale.setlocale(locale.LC_ALL, '')
 sys.modules['django.contrib.auth.tests'] = None
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-    from django.core.management import execute_from_command_line
-    execute_from_command_line(sys.argv)
+    execute_manager(settings)
