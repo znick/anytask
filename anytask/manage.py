1414183fb88e8e27debdf7aa73daee89e732dad9
HolyPrapor <tolya12345w@gmail.com>
2020-04-08 02:21:45 +0500

    Fixing import problems

--- anytask/manage.py
+++ anytask/manage.py
@@ -5,6 +5,8 @@ import sys
 import os
 import imp
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 
 locale.setlocale(locale.LC_ALL, '')
 
@@ -22,7 +24,7 @@ except ImportError:
 sys.modules['django.contrib.auth.tests'] = None
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "anytask.settings")
 
     from django.core.management import execute_from_command_line
 
