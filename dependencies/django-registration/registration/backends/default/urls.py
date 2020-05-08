5d0a2ed144e7fb10677234a2873c049af5621237
HolyPrapor <tolya12345w@gmail.com>
2020-05-08 16:26:41 +0500

    Fixed 'RU' key problem, faced new ones

--- dependencies/django-registration/registration/backends/default/urls.py
+++ dependencies/django-registration/registration/backends/default/urls.py
@@ -17,35 +17,35 @@ up your own URL patterns for these views instead.
 
 """
 
-
 from django.conf.urls import *
 from django.views.generic.base import TemplateView
 
 from registration.views import activate
 from registration.views import register
 
-
-urlpatterns = patterns('',
-                       url(r'^activate/complete/$',
-                           TemplateView.as_view(template_name='registration/activation_complete.html'),
-                           name='registration_activation_complete'),
-                       # Activation keys get matched by \w+ instead of the more specific
-                       # [a-fA-F0-9]{40} because a bad activation key should still get to the view;
-                       # that way it can return a sensible "invalid key" message instead of a
-                       # confusing 404.
-                       url(r'^activate/(?P<activation_key>\w+)/$',
-                           activate,
-                           {'backend': 'registration.backends.default.DefaultBackend'},
-                           name='registration_activate'),
-                       url(r'^register/$',
-                           register,
-                           {'backend': 'registration.backends.default.DefaultBackend'},
-                           name='registration_register'),
-                       url(r'^register/complete/$',
-                           TemplateView.as_view(template_name='registration/registration_complete.html'),
-                           name='registration_complete'),
-                       url(r'^register/closed/$',
-                           TemplateView.as_view(template_name='registration/registration_closed.html'),
-                           name='registration_disallowed'),
-                       (r'', include('registration.auth_urls')),
-                       )
+urlpatterns = [url(r'^activate/complete/$',
+                   TemplateView.as_view(
+                       template_name='registration/activation_complete.html'),
+                   name='registration_activation_complete'),
+               # Activation keys get matched by \w+ instead of the more specific
+               # [a-fA-F0-9]{40} because a bad activation key should still get to the view;
+               # that way it can return a sensible "invalid key" message instead of a
+               # confusing 404.
+               url(r'^activate/(?P<activation_key>\w+)/$',
+                   activate,
+                   {'backend': 'registration.backends.default.DefaultBackend'},
+                   name='registration_activate'),
+               url(r'^register/$',
+                   register,
+                   {'backend': 'registration.backends.default.DefaultBackend'},
+                   name='registration_register'),
+               url(r'^register/complete/$',
+                   TemplateView.as_view(
+                       template_name='registration/registration_complete.html'),
+                   name='registration_complete'),
+               url(r'^register/closed/$',
+                   TemplateView.as_view(
+                       template_name='registration/registration_closed.html'),
+                   name='registration_disallowed'),
+               url(r'', include('registration.auth_urls')),
+               ]
