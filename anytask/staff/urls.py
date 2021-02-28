40eb0b597b6de52c8c69261c9b24e8154b3a11ae
bcskda <demulkup@ya.ru>
2021-02-28 16:31:07 +0300

    #333 urls.W002

--- anytask/staff/urls.py
+++ anytask/staff/urls.py
@@ -7,10 +7,10 @@ urlpatterns = (
         name="staff.views.ajax_change_status"),
     url(r'^ajax_save_ids', staff.views.ajax_save_ids,
         name="staff.views.ajax_save_ids"),
-    url(r'/gradebook/$', staff.views.get_gradebook,
+    url(r'gradebook/$', staff.views.get_gradebook,
         name="staff.views.get_gradebook"),
-    url(r'/gradebook/(?P<statuses>\w+)$', staff.views.gradebook_page,
+    url(r'gradebook/(?P<statuses>\w+)$', staff.views.gradebook_page,
         name="staff.views.gradebook_page"),
-    url(r'/gradebook_page', staff.views.gradebook_page,
+    url(r'gradebook_page', staff.views.gradebook_page,
         name="staff.views.gradebook_page")
 )
