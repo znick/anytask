7e720ba8edfcee7a11fc2fbd8196d325e5f99291
Mikhail Yumanov <yumanov.ms@phystech.edu>
2021-03-06 12:50:26 +0300

    Working version.
    Moved deploy to deploy_local_beta. Moved deploy in separate files, run and cleanup deal only with environment.

---
+++ deploy_local_beta/deploy_local_beta_python2.sh
@@ -0,0 +1,39 @@
+# This file must not be run directly.
+# Execute `deploy_local_beta/run.sh` instead.
+
+
+if ! test $ANYBETA_ROOT
+then
+  echo "ERROR: This file should not be run directly."
+  echo "Execute \`deploy_local_beta/run.sh\` instead."
+  exit 1
+fi
+
+
+#SETUP VIRTUALENV
+#################
+
+ANYBETA_report
+ANYBETA_report "Enable virtualenv"
+virtualenv -p $ANYBETA_PYTHON_PATH $ANYBETA_VENV_NAME
+. $ANYBETA_VENV_ACTIVATE
+
+
+# REQUIREMENTS
+##############
+
+ANYBETA_report
+ANYBETA_report "Install requirements"
+pip install 'pip>=20.3.4' 'setuptools>=44.0.0' --upgrade
+pip install -r requirements_local.txt
+
+
+# MANAGE DJANGO
+###############
+
+ANYBETA_report
+ANYBETA_report "Manage django project"
+$ANYBETA_PYTHON_PATH setup.py develop
+./anytask/manage.py
+./anytask/manage.py migrate --noinput
+#./anytask/manage.py runserver 127.0.0.1:8019 -v 3 --traceback
