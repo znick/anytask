418dbf88dfe07f15d2a93de8c453de47efb3819f
Dmitrii Kuptsov <demulkup@ya.ru>
2021-03-27 16:03:41 +0300

    CST-27 Use common.s3_migrate in s3migrate_user_avatars

--- anytask/users/management/commands/s3migrate_user_avatars.py
+++ anytask/users/management/commands/s3migrate_user_avatars.py
@@ -1,51 +1,27 @@
 # -*- coding: utf-8 -*-
 
-from django.core.management.base import BaseCommand
-
-from storage import S3OverlayStorage, migrate_to_s3
+from common.s3_migrate import S3MigrateCommand
 from users.models import UserProfile
 
 
-class Command(BaseCommand):
-    help = """Upload user avatars to S3 cloud.
-    Won't delete files from local storage.
-    """
+class Command(S3MigrateCommand):
+    help = """
+        Upload user avatars to S3 cloud.
+        Won't delete files from local storage.
+        """
 
-    def add_arguments(self, parser):
-        parser.add_argument(
-            '--execute',
-            dest='execute',
-            action='store_true',
-            default=False,
-            help='Actually execute migration')
-        '''
-        parser.add_argument(
-            '--reverse',
-            dest='reverse',
-            action='store_true',
-            default=False,
-            help='DEV ONLY')
-        '''
+    def all_models(self, options):
+        return UserProfile.objects.all()
 
-    def handle(self, **options):
-        dry_run = not options['execute']
+    def get_model_field(self, model, options):
+        return model.avatar
 
-        if dry_run:
-            print("NOTE: Dry run")
-        dest_storage = S3OverlayStorage()
-
-        for user_model in UserProfile.objects.all():
-            if not user_model.avatar:
-                continue
-            try:
-                new_path = migrate_to_s3(user_model.avatar, dest_storage, dry_run)
-                print("Note: uploaded: {}".format(new_path))
-            except KeyError as e:
-                new_path = e.message
-                print("Note: destination already exists: {}".format(new_path))
-            except Exception as e:
-                print("Unhandled exception, ignoring: {}".format(e))
-                continue
-            if not dry_run:
-                user_model.avatar.name = new_path
-                user_model.save()
+    def update_model(self, model, new_path, options):
+        try:
+            model.avatar.name = new_path
+            model.save()
+        except Exception as e:
+            print('Unhandled exception in {}: {}'.format(self.update_model, e))
+            return False
+        else:
+            return True
