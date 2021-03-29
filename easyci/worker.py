380f02d2b4f2eec972a7e5c5e1eac3c8cd631f69
Mikhail Yumanov <yumanov.m@gmail.com>
2021-03-29 18:00:25 +0300

    CST-37 znick#325 add scheduler, move old docker.execute to new worker

--- easyci/worker.py
+++ easyci/worker.py
@@ -1,44 +1,74 @@
-import requests
-import json
+import logging
+import os
+import shutil
+import subprocess
+import tempfile
 
-from settings_local import GITHUB_TOKEN, \
-    GITHUB_USER, GITHUB_REPO, GITHUB_ORG, GITHUB_WORKFLOW
+from contextlib import contextmanager
+from urllib.request import urlretrieve
+
+# custom
+import docker
+
+TASK_DIR = "task"
+GIT_DIR = "git"
+DOCKER_WORKDIR = "/task_dir"
+DOCKER_TASK_DIR = '/'.join((DOCKER_WORKDIR, TASK_DIR))
+DOCKER_GIT_DIR = '/'.join((DOCKER_WORKDIR, GIT_DIR))
 
 
-class AbstractWorker:
-    def __init__(self, auth, task, repo, run_cmd, files, docker_image, timeout):
-        self.auth = auth
+@contextmanager
+def tmp_dir():
+    t = tempfile.mkdtemp(dir="/var/tmp")
+    try:
+        yield t
+    finally:
+        shutil.rmtree(t)
+
+
+def git_clone(repo, dst_dir):
+    cmd = ["git", "clone", repo, dst_dir]
+    logging.info("RUN: %s", cmd)
+    subprocess.check_call(cmd)
+
+
+def prepare_dir(repo, files, dirname="./"):
+    git_dir = os.path.join(dirname, GIT_DIR)
+    task_dir = os.path.join(dirname, TASK_DIR)
+    git_clone(repo, git_dir)
+
+    os.mkdir(task_dir)
+    for url in files:
+        # FIXME: do it using os
+        filename = url.split('/')[-1]
+        dst_path = os.path.join(task_dir, filename)
+        logging.info("Download '%s' -> '%s'", url, dst_path)
+        urlretrieve(url, dst_path)
+
+
+class Worker:
+    def __init__(self, task, repo, run_cmd, files, docker_image, timeout):
         self.task = task
         self.repo = repo
         self.run_cmd = run_cmd
         self.files = files
         self.docker_image = docker_image
-        self.timeout = str(timeout)
+        self.timeout = timeout
+        logging.info("init finished")
 
     def run(self):
-        raise NotImplementedError("attempt to run abstract worker.")
+        logging.info("Proccess task %s", self.task)
+        with tmp_dir() as dirname:
+            prepare_dir(self.repo, self.files, dirname)
+            run_cmd = [self.run_cmd] + [self.task, DOCKER_TASK_DIR]
+            ret = docker.execute(
+                    run_cmd,
+                    cwd=DOCKER_GIT_DIR, 
+                    timeout=self.timeout, 
+                    user='root',
+                    network='bridge', 
+                    image=self.docker_image,
+                    volumes=["{}:{}:ro".format(
+                        os.path.abspath(dirname), DOCKER_WORKDIR)])
 
-
-class GithubActionsWorker(AbstractWorker):
-    def __init__(self, *args):
-        super().__init__(*args)
-
-    def run(self):
-        inputs = {"task" : self.task,
-                "repo" : self.repo,
-                "run_cmd" : self.run_cmd,
-                "docker_image" : self.docker_image,
-                "timeout" : self.timeout}
-        data = {"ref" : "master", "inputs" : inputs}
-        headers = {"Accept" : "application/vnd.github.v3+json"}
-        url = "https://api.github.com/repos/{}/{}/actions/workflows/{}/" \
-              "dispatches".format(GITHUB_ORG, GITHUB_REPO, GITHUB_WORKFLOW)
-        auth = (GITHUB_USER, GITHUB_TOKEN)
-        r = requests.post(url, data=json.JSONEncoder().encode(data),
-                headers=headers, auth=auth)
-        return r
-
-    def assert_repo_form(self):
-        # TODO: check if 'repo' has form '<owner>/<repo_name>'
-        # throw TypeError if it does not
-        return True
+            return ret
