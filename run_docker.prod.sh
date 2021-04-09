f9494c9875e262d1fca221b7d621e101f059cf02
houcha <baiuk.is@phystech.edu>
2021-04-10 01:43:51 +0300

    CST-69 add prod reviewboard; nginx doesn't serve static files for rb

--- run_docker.prod.sh
+++ run_docker.prod.sh
@@ -1,17 +1,25 @@
 # remove the volumes along with the containers
 docker-compose down -v
-# # as previous (should try this in hopeless cases)
-# docker volume rm anytask_static_volume anytask_media_volume anytask_mysql_data anytask_media
 # # clear cache
-# docker system prune -a
+#docker system prune -a
 
 docker-compose -f docker-compose.prod.yml up -d --build
+
+# TODO: use wait-for-it instead
 sleep 10s
-docker-compose -f docker-compose.prod.yml exec web python anytask/manage.py makemigrations
-docker-compose -f docker-compose.prod.yml exec web python anytask/manage.py migrate --noinput
-docker-compose -f docker-compose.prod.yml exec web python anytask/manage.py collectstatic --no-input --clear
+
+docker-compose -f docker-compose.prod.yml exec anytask python anytask/manage.py makemigrations
+docker-compose -f docker-compose.prod.yml exec anytask python anytask/manage.py migrate --noinput
+docker-compose -f docker-compose.prod.yml exec anytask python anytask/manage.py collectstatic --no-input --clear
 
 # TODO: move to docker-compose
-docker-compose -f docker-compose.prod.yml exec db bash -c "mysql --user=root --password=\"\$MYSQL_ROOT_PASSWORD\" --execute=\"GRANT ALL PRIVILEGES ON test_\$MYSQL_DATABASE.* TO '\$MYSQL_USER'@'%';\""
-docker-compose -f docker-compose.prod.yml exec web python anytask/manage.py test
+docker-compose -f docker-compose.prod.yml exec db bash -c "mysql --user=root --password=\"\$MYSQL_ROOT_PASSWORD\"\
+    --execute=\"
+    CREATE DATABASE \$DATABASE_NAME;                                            # reviewboard db
+    CREATE USER '\$DATABASE_USERNAME'@'%' IDENTIFIED BY '\$DATABASE_PASSWORD';  # reviewboard user
+    GRANT ALL PRIVILEGES ON \$DATABASE_NAME.* TO '\$DATABASE_USERNAME'@'%';
+    GRANT ALL PRIVILEGES ON test_\$MYSQL_DATABASE.* TO '\$MYSQL_USER'@'%';      # for manage.py test
+    \""
+
+docker-compose -f docker-compose.prod.yml exec anytask python anytask/manage.py test
 
--- run_docker.sh
+++ run_docker.sh
@@ -13,7 +13,7 @@ docker-compose exec db bash -c "mysql --user=root --password=\"\$MYSQL_ROOT_PASS
     CREATE DATABASE \$DATABASE_NAME;                                            # reviewboard db
     CREATE USER '\$DATABASE_USERNAME'@'%' IDENTIFIED BY '\$DATABASE_PASSWORD';  # reviewboard user
     GRANT ALL PRIVILEGES ON \$DATABASE_NAME.* TO '\$DATABASE_USERNAME'@'%';
-    GRANT ALL PRIVILEGES ON test_\$MYSQL_DATABASE.* TO '\$MYSQL_USER'@'%';
+    GRANT ALL PRIVILEGES ON test_\$MYSQL_DATABASE.* TO '\$MYSQL_USER'@'%';      # for manage.py test
     \""
 
 docker-compose exec anytask python manage.py test
