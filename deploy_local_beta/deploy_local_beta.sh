8de47232065cf6e5b8ed645e9861ab3d8b8c2703
Dmitrii Kuptsov <demulkup@ya.ru>
2021-04-17 10:36:49 +0300

    CST-46 Optional MinIO container in deploy_local_beta

--- deploy_local_beta/deploy_local_beta.sh
+++ deploy_local_beta/deploy_local_beta.sh
@@ -31,3 +31,28 @@ then
 else
   . $ANYBETA_DEPLOY/deploy_local_beta_python3.sh
 fi
+
+# MINIO
+################
+
+if [ x"$ANYBETA_WITH_MINIO" = "x1" ]
+then
+  ANYBETA_report
+  ANYBETA_report "Pull MinIO docker image"
+  docker pull "$ANYBETA_MINIO_IMAGE"
+  ANYBETA_crash_on_error
+  ANYBETA_report "Start MinIO container"
+  docker run --rm --detach -p 9000:9000 "$ANYBETA_MINIO_IMAGE" server /data
+  sleep 5
+  ANYBETA_crash_on_error
+  ANYBETA_report "Create test bucket"
+  s3cmd --no-ssl $ANYBETA_MINIO_OPTIONS mb s3://"$ANYBETA_MINIO_BUCKET"
+  ANYBETA_crash_on_error
+fi
+
+# FINISHED
+#################
+
+ANYBETA_report
+ANYBETA_report "Deploy completed!"
+ANYBETA_report "You can now start django server using \`manage.py runserver\`"
