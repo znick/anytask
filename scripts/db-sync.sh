fd66de85290e8851ef25a594409b18e2f97ced3f
houcha <baiuk.is@phystech.edu>
2021-05-08 12:39:47 +0300

    move db sync from maatkit to percona

--- scripts/db-sync.sh
+++ scripts/db-sync.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 
-# TODO: fix `Redundant argument in sprintf at /usr/bin/mk-table-sync line 28.` warning?
-/usr/bin/mk-table-sync --charset utf8 --execute \
-    'u=$MYSQL_USER,p=$MYSQL_PASSWORD,h=127.0.0.1,D=$MYSQL_DATABASE,t=auth_user'\
-    'u=$DATABASE_NAME,p=$DATABASE_PASSWORD,h=127.0.0.1,D=DATABASE_NAME,t=auth_user'
-
+/usr/bin/pt-table-sync -v --user=root --password=$MYSQL_ROOT_PASSWORD --charset utf8 --execute \
+    h=127.0.0.1,D=$MYSQL_DATABASE,t=auth_user \
+    h=127.0.0.1,D=$DATABASE_NAME,t=auth_user
