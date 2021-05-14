c67ee08a3ff38bcef9c7a66858170806206a36fe
houcha <baiuk.is@phystech.edu>
2021-05-15 00:54:11 +0300

    suppress nginx cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -4,6 +4,6 @@
 docker-compose down -v --remove-orphans
 # clear cache if necessary
 docker system prune -af
-# stop nginx daemon
-sudo nginx -c /etc/nginx/nginx_anytask.conf -s stop
+# stop nginx daemon if exists
+sudo nginx -c /etc/nginx/nginx_anytask.conf -s stop &> /dev/null
 
