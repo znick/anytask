c6e9da6054fcd9f7fbfe1aae9aa89c3fecc549f2
houcha <baiuk.is@phystech.edu>
2021-05-14 19:06:08 +0300

    adjust run_docker script

--- run_docker.sh
+++ run_docker.sh
@@ -1,16 +1,18 @@
-docker-compose up -d --build
+#!/bin/bash
 
-docker-compose exec db ./usr/src/init.sh
-docker-compose exec anytask sh -c "
+# build and run
+docker-compose up -d --build
+# init db
+docker-compose exec -T db ./usr/src/init.sh
+# init anytask
+docker-compose exec -T anytask sh -c "
     cd anytask\
     && python manage.py makemigrations --settings=anytask.settings_production\
     && python manage.py migrate --noinput --settings=anytask.settings_production\
     && python manage.py collectstatic --no-input --clear --settings=anytask.settings_production\
     && crond"
-
 # to provide script with proxy_params and stuff like this
 sudo cp configs/nginx/nginx_anytask.conf /etc/nginx/
+# start nginx
 sudo nginx -c /etc/nginx/nginx_anytask.conf
-
-# to stop daemon run: `sudo nginx -c /etc/nginx/nginx_anytask.conf -s stop`
 
