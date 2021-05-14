4a092a2c6f97da28c80ec668dc4c8ac149d333d1
houcha <baiuk.is@phystech.edu>
2021-05-15 02:16:50 +0300

    fix cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -6,4 +6,8 @@ docker-compose down -v --remove-orphans
 docker system prune -af
 # stop nginx daemon if exists
 sudo nginx -c /etc/nginx/nginx_anytask.conf -s stop
+# remove previously mounter volumes manually, otherwise get
+# `django.db.utils.ProgrammingError: (1146, "Table 'rb_db.django_site' doesn't exist")`
+# FIXME: too bad, but i haven't come up with a better solution
+sudo rm -rf /var/lib/anytask /var/www/reviewboard
 
