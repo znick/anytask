c72a03dfb68282998447cc4fdc4b38f5912ecb78
Nikolay Zhuravlev <znick@znick.ru>
2021-10-03 00:04:21 +0300

    Fix perm

--- easyci2/gitlabci/worker.py
+++ easyci2/gitlabci/worker.py
@@ -25,7 +25,7 @@ def git_clone(repo, dst_dir):
     ssh_key = os.environ.get("SSH_KEY")
     ssh = "ssh -o StrictHostKeyChecking=no -vvv"
     if ssh_key:
-#        subprocess.check_call(["chmod", "0600", ssh_key])
+        subprocess.check_call(["chmod", "0600", ssh_key])
         ssh += " -i '{}'".format(ssh_key)
 
     env = os.environ.copy()
