68a7f748d825d54e88e48799040ad630531992cc
Nikolay Zhuravlev <znick@znick.ru>
2024-03-02 16:09:09 +0100

    Add mark_safe to INDEX_MESSAGE and ALL_PAGES_MESSAGE

--- anytask/index/templatetags/index_messages.py
+++ anytask/index/templatetags/index_messages.py
@@ -1,14 +1,15 @@
 from django import template
 from django.conf import settings
+from django.utils.html import mark_safe
 
 register = template.Library()
 
 
 @register.simple_tag
 def index_message():
-    return getattr(settings, 'INDEX_MESSAGE', '')
+    return mark_safe(getattr(settings, 'INDEX_MESSAGE', ''))
 
 
 @register.simple_tag
 def all_pages_message():
-    return getattr(settings, 'ALL_PAGES_MESSAGE', '')
+    return mark_safe(getattr(settings, 'ALL_PAGES_MESSAGE', ''))
