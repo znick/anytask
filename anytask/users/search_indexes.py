d43e0185bc59956552161cb82df4fad3357b6166
Nikolay Zhuravlev <zhnick@zhnick.me>
2025-09-01 20:20:02 +0200

    Add defaults for search index

--- anytask/users/search_indexes.py
+++ anytask/users/search_indexes.py
@@ -9,8 +9,8 @@ class UserProfileIndex(indexes.SearchIndex, indexes.Indexable):
 
     fullname_auto = indexes.NgramField()
     login_auto = indexes.NgramField(model_attr='user__username')
-    ya_contest_login_auto = indexes.NgramField(model_attr='ya_contest_login')
-    ya_passport_email_auto = indexes.NgramField(model_attr='ya_passport_email')
+    ya_contest_login_auto = indexes.NgramField(model_attr='ya_contest_login', default='')
+    ya_passport_email_auto = indexes.NgramField(model_attr='ya_passport_email', default='')
     email_auto = indexes.NgramField(model_attr='user__email')
 
     def get_model(self):
